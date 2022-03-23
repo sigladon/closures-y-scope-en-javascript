152dfa42d98359b4dc1f14fe1628ef62fedae15d
sigladon <sigladon.rc@gmail.com>
2022-03-22 20:00:22 -0500

    Global Scope

---
+++ src/scope/global.js
@@ -0,0 +1,25 @@
+var hello = 'Hello';
+let world = 'Hello World';
+const helloWorld = 'Hello World!';
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFunction();
+
+const helloWorld = () => {
+    globalVar = 'im global'
+}
+
+helloWorld();
+console.log(globalVar);
+
+const anotherFunction = () => {
+    var localVar = globalVar = 'Im Global';
+}
+
+anotherFunction();
+console.log(globalVar);
